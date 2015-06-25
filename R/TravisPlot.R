55d2facaa8f4d5a2d549b44a891da9075b5b8caf
lzcyzm <jia.meng@hotmail.com>
2015-06-25 11:13:53 +0800

    version 0.02
    density

--- R/TravisPlot.R
+++ R/TravisPlot.R
@@ -1,6 +1,6 @@
 
 
-TravisPlot <- function(peak,TravisCoords=NA,txdb=NA,genome=NA,saveToPDFprefix=NA){
+TravisPlot <- function(peak,TravisCoordsFromTxDb=NA,txdb=NA,genome=NA,saveToPDFprefix=NA){
   
   # make sure the travis coordinates are available
   if (is.na(TravisCoords)&is.na(txdb)&is.na(genome)) {
@@ -19,6 +19,7 @@ TravisPlot <- function(peak,TravisCoords=NA,txdb=NA,genome=NA,saveToPDFprefix=NA
     }
   } else {
     print("Using provided Travis Coordinates")
+    TravisCoords <- TravisCoordsFromTxDb
   }
   
   # import bed12 file
@@ -33,37 +34,38 @@ TravisPlot <- function(peak,TravisCoords=NA,txdb=NA,genome=NA,saveToPDFprefix=NA
     peak[[i]] =temp
   }
   ct=.combineListOfDataFrame(peak)
-  ct[[5]] <- as.character(ct[[5]])
+  ct[[4]] <- as.character(ct[[4]])
   
   # plot figure
-  ct1 <- ct[ct$category=="mRNA",]
-  ct2 <- ct[ct$category=="lncRNA",]
-  save(ct1,ct2,file="TravisPlot.RData")
-  p1 <- ggplot(ct1, aes(x=pos, y=Freq, group=Feature)) + 
+  ct1 <- ct[(ct$category=="mRNA")&(ct$count==1),]
+  ct2 <- ct[(ct$category=="lncRNA")&(ct$count==1),]
+  
+  # save(ct1,ct2,file="TravisPlot.RData")
+  p1 <- ggplot(ct1, aes(x=pos, group=Feature)) + 
     ggtitle("Distribution on mRNA") +
     theme(axis.ticks = element_blank(), axis.text.x = element_blank()) + xlab("") + ylab("Frequency") +
-    annotate("pointrange", x = 1, y = 0.3, ymin = 0.1, ymax = 0.5, colour = "black",size=1) + 
-    annotate("pointrange", x = 2, y = 0.3, ymin = 0.1, ymax = 0.5, colour = "black",size=1) + 
-    annotate("pointrange", x = 0, y = 0.3, ymin = 0.1, ymax = 0.5, colour = "black",size=1) + 
-    annotate("pointrange", x = 3, y = 0.3, ymin = 0.1, ymax = 0.5, colour = "black",size=1) + 
-    annotate("rect", xmin = 0, xmax = 1, ymin = 0.15, ymax = 0.4, alpha = .2, fill = "blue") +
-    annotate("rect", xmin = 1, xmax = 2, ymin = 0.15, ymax = 0.4, alpha = .2, fill = "green") +
-    annotate("rect", xmin = 2, xmax = 3, ymin = 0.15, ymax = 0.4, alpha = .2, fill = "red") +
-    geom_line(aes(colour = Feature),size=2) +
-    annotate("text", x = 0.5, y = 0.3, label = "5'UTR") +
-    annotate("text", x = 1.5, y = 0.3, label = "CDS") +
-    annotate("text", x = 2.5, y = 0.3, label = "3'UTR")
+    annotate("pointrange", x = 1/3, y = -0.3, ymin = -0.5, ymax = -0.1, colour = "black",size=1) + 
+    annotate("pointrange", x = 2/3, y = -0.3, ymin = -0.5, ymax = -0.1, colour = "black",size=1) + 
+    annotate("pointrange", x = 0/3, y = -0.3, ymin = -0.5, ymax = -0.1, colour = "black",size=1) + 
+    annotate("pointrange", x = 3/3, y = -0.3, ymin = -0.5, ymax = -0.1, colour = "black",size=1) + 
+    annotate("rect", xmin = 0/3, xmax = 1/3, ymin = -0.4, ymax = -0.2, alpha = .2, fill = "blue") +
+    annotate("rect", xmin = 1/3, xmax = 2/3, ymin = -0.4, ymax = -0.2, alpha = .2, fill = "green") +
+    annotate("rect", xmin = 2/3, xmax = 3/3, ymin = -0.4, ymax = -0.2, alpha = .2, fill = "red") +
+    geom_density(aes(fill=factor(Feature)),alpha=0.2) +
+    annotate("text", x = 0.5/3, y = -0.3, label = "5'UTR") +
+    annotate("text", x = 1.5/3, y = -0.3, label = "CDS") +
+    annotate("text", x = 2.5/3, y = -0.3, label = "3'UTR")
   
-  p2 <- ggplot(ct1, aes(x=pos, y=Freq, group=Feature)) + 
+  p2 <- ggplot(ct2, aes(x=pos, group=Feature)) + 
     ggtitle("Distribution on lncRNA") +
     theme(axis.ticks = element_blank(), axis.text.x = element_blank()) + xlab("") + ylab("Frequency") +
-    annotate("pointrange", x = 0, y = 0.3, ymin = 0.1, ymax = 0.5, colour = "black",size=1) + 
-    annotate("pointrange", x = 3, y = 0.3, ymin = 0.1, ymax = 0.5, colour = "black",size=1) + 
-    annotate("rect", xmin = 0, xmax = 3, ymin = 0.15, ymax = 0.4, alpha = .2, fill = "green") +
-    geom_line(aes(colour = Feature),size=2) +
-    annotate("text", x = 0.3, y = 0.3, label = "5'End") +
-    annotate("text", x = 1.5, y = 0.3, label = "lncRNA") +
-    annotate("text", x = 2.7, y = 0.3, label = "3'End") 
+    annotate("pointrange", x = 0/3, y = -0.3, ymin = -0.5, ymax = -0.1, colour = "black",size=1) + 
+    annotate("pointrange", x = 3/3, y = -0.3, ymin = -0.5, ymax = -0.1, colour = "black",size=1) + 
+    annotate("rect", xmin = 0/3, xmax = 3/3, ymin = -0.4, ymax = -0.2, alpha = .2, fill = "green") +
+    geom_density(aes(fill=factor(Feature)),alpha=0.2) +
+    annotate("text", x = 0.3/3, y = -0.3, label = "5'End") +
+    annotate("text", x = 1.5/3, y = -0.3, label = "lncRNA") +
+    annotate("text", x = 2.7/3, y = -0.3, label = "3'End") 
   
   if (is.na(saveToPDFprefix)) {
     # return the result
@@ -87,36 +89,8 @@ TravisPlot <- function(peak,TravisCoords=NA,txdb=NA,genome=NA,saveToPDFprefix=NA
   # count overlaps
   n <- countOverlaps(TravisCoords,peak)
   q <- data.frame(mcols(TravisCoords),count=n)
+  return(q)
   
-  # divide by mRNA and lncRNA
-  mrna <- q[(q$category=="mRNA")&(q$count>0),]
-  lncrna <- q[(q$category=="lncRNA")&(q$count>0),]
-  
-  # process mRNA
-  mrna$comp <- as.character(mrna$comp)
-  result <- table(mrna[,2:3])
-  mrna <- as.data.frame(result)
-  mrna$Freq <- mrna$Freq/mean(mrna$Freq)/3
-  mrna <- data.frame(mrna,category="mRNA")
-  mrna <- mrna[(mrna$Freq>0.0000000001),]
-  
-  
-  # process lncRNA
-  lncrna$comp <- as.character(lncrna$comp)
-  result <- table(lncrna[,c(2,3)])
-  lncrna <- as.data.frame(result)
-  lncrna$Freq <- lncrna$Freq/mean(lncrna$Freq)
-  lncrna <- data.frame(lncrna,category="lncRNA")
-  
-  
-  # output
-  out <- rbind(mrna,lncrna)
-  out$pos <- as.numeric(as.character(out$pos))
-  out$comp <- factor(as.character(out$comp),levels=c("UTR5","CDS","UTR3","lncRNA"))
-  out$pos <- out$pos*3
-  
-  # return
-  return(out)
 }
 
 .combineListOfDataFrame <- function(t){
